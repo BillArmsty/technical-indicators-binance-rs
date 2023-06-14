ca19ee2ce5982bf0f0d16003640ddb1df461999b
Bill Armstrong <armsty9@gmail.com>
2023-06-14 13:07:57 +0300

    Implemented simple moving average

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,26 @@ mod utils;
 #[tokio::main]
 async fn main() {
     let client = utils::get_client();
-    let result = binance::get_klines(client.clone(), "1d", "BTCUSDT", 500).await;
+    let result = binance::get_klines(client.clone(), "1m", "BTCUSDT", 500).await;
 
     let kline_data = match result {
         Some(kline_data) => kline_data,
         _ => { panic!("Failed to get kline data") }
     };
     println!("first result: {:?}", kline_data[0]);
+
+    // Get Vec of f64 based on the close prices for the period. We reverse the order of the vec with rev() and then take(100) to take the 100 latest values
+    let price_data: Vec<f64> = kline_data
+        .iter()
+        .rev()
+        .take(100)
+        .map(|x| x.close)
+        .collect();
+    let result = statistics::simple_moving_average(&price_data, 26);
+
+    let sma_data = match result {
+        Some(sma_data) => sma_data,
+        _ => { panic!("Failed to get SMA data") }
+    };
+    println!("SMAs: {:?}", sma_data);
 }
